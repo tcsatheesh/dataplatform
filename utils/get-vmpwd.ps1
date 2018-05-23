f219beee7d68dc1378bd334b8e866dda9db7acb2
tcsatheesh <tc.satheesh@microsoft.com>
2018-05-23 12:37:02 +0100

    Removing utils for now. Need a  tidier version.

--- utils/get-vmpwd.ps1
+++
@@ -1,3 +0,0 @@
-# Get-AzureKeyVaultSecret -VaultName itiacdnekvltacc -Name itiacdvm01-vmadmin-password
-# Get-AzureKeyVaultSecret -VaultName itiacdnekvltacc -Name itiacdvm01-clsadmin-password
-Get-AzureKeyVaultSecret -VaultName itiacdnekvltacc -Name it-iac-d-app-prn-password
