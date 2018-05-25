3871d806e61d865fc84a1e69c249087ed6945e63
tcsatheesh <tc.satheesh@microsoft.com>
2018-05-25 17:37:20 +0100

    added sendgrid but it doesn't work due to market place terms

--- common/ps/Get-CommonFunctions.ps1
+++ common/ps/Get-CommonFunctions.ps1
@@ -150,13 +150,13 @@ function Update-ProjectParameters {
         [Parameter(Mandatory = $True, HelpMessage = 'The name of the parameter file.')]
         [string]$parameterFileName
     )
-    Write-Verbose "Projects Parameter File $projectsParameterFile"
+    # Write-Verbose "Projects Parameter File $projectsParameterFile"
     $projectFolder = (Get-Item -Path $projectsParameterFile).DirectoryName
-    Write-Verbose "Projects Folder $projectFolder"
+    # Write-Verbose "Projects Folder $projectFolder"
     $projectParameterFullPath = "$projectFolder\$parameterFileName"
-    Write-Verbose "Project Parameter Full Path: $projectParameterFullPath"
+    # Write-Verbose "Project Parameter Full Path: $projectParameterFullPath"
     $parameters | ConvertTo-JSON -Depth 10 | Out-File -filepath $projectParameterFullPath -Force -Encoding utf8
-    Write-Verbose "Project parameter file updated at: $projectParameterFullPath"
+    # Write-Verbose "Project parameter file updated at: $projectParameterFullPath"
 }
 
 function Set-Subscription {
