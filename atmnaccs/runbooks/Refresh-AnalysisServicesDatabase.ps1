b6e21f5122ad3a05470518514acb3d7a933f09aa
tcsatheesh <tc.satheesh@microsoft.com>
2019-04-08 15:58:47 +0100

    bug fix

--- atmnaccs/runbooks/Refresh-AnalysisServicesDatabase.ps1
+++ atmnaccs/runbooks/Refresh-AnalysisServicesDatabase.ps1
@@ -4,7 +4,7 @@ $TenantId = Get-AutomationVariable -Name "TenantId"
 $databaseName = Get-AutomationVariable -Name "AnalysisServicesDatabaseName"
 $analysisServer = Get-AutomationVariable -Name "AnalysisServicesServer"
 $analysisSeverRolloutEnvironment = Get-AutomationVariable -Name "AnalysisServicesRolloutEnvironment"
-$refreshType = Get-AzureAutomationVariable -Name "DatabaseRefreshType"
+$refreshType = Get-AutomationVariable -Name "DatabaseRefreshType"
 
 Add-AzureAnalysisServicesAccount -Credential $Credential -ServicePrincipal -TenantId $TenantId -RolloutEnvironment $analysisSeverRolloutEnvironment
 Invoke-ProcessASDatabase -DatabaseName $databaseName -RefreshType $refreshType -Server $analysisServer
