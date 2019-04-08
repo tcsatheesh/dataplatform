cae099f20b13bf7b149067d35103c88a721f6d57
tcsatheesh <tc.satheesh@microsoft.com>
2019-04-08 21:54:30 +0100

    added webhook uri support

--- atmnaccs/ps/New-Resources.ps1
+++ atmnaccs/ps/New-Resources.ps1
@@ -4,7 +4,110 @@ param
     [String]$projectsParameterFile
 )
 
-function Get-Values($value) {
+function Get-WebhookUri {
+    param(
+        [string]$resourcename
+    )
+    $subscriptionId = Get-SubscriptionId
+    $tenantId = (Get-ProjectParameter -type "tenant").id
+    $client = Get-ApplicationParameter -type "applicationPrincipal" -godeep
+    $clientId = $client.application.clientId
+    $clientSecretName = $client.application.passwordSecretName
+    $keyVaultName = Get-KeyVaultName -keyVaultType "appkeyvault"
+    Write-Verbose "Key Vault name: $keyVaultName"
+    $secret = Get-AzureKeyVaultSecret -VaultName $keyVaultName -Name $clientSecretName
+    $clientSecret = $secret.SecretValueText
+    $resourceGroupName = Get-ResourceGroupName -resourceGroupTypeRef "app"
+    $automationAccountName = $resourcename
+
+    $parameters = @{
+        "subscriptionId" = $subscriptionId
+        "tenantId" = $tenantId
+        "clientId" = $clientId
+        "clientSecret" = $clientSecret
+        "resourceGroupName" = $resourceGroupName
+        "automationAccountName" = $automationAccountName
+    }
+    
+    $val = Get-WebhookUriInternal   -subscriptionId $parameters.subscriptionId `
+                                    -tenantId $parameters.tenantId `
+                                    -clientId $parameters.clientId `
+                                    -clientSecret $parameters.clientSecret `
+                                    -resourceGroupName $parameters.resourceGroupName `
+                                    -automationAccountName $parameters.automationAccountName
+    return $val
+}
+
+
+function Get-WebhookUriInternal {
+    param (
+        [Parameter(Mandatory = $True, HelpMessage = 'Subscription id')]
+        [String]$subscriptionId,
+    
+        [Parameter(Mandatory = $True, HelpMessage = 'Tenant id')]
+        [String]$tenantId,
+    
+        [Parameter(Mandatory = $True, HelpMessage = 'Specify the client Id.')]
+        [String]$clientId,
+        
+        [Parameter(Mandatory = $True, HelpMessage = 'Specify the client secret.')]
+        [String]$clientSecret,
+    
+        [Parameter(Mandatory = $True, HelpMessage = 'Specify the resource to get access token for')]
+        [String]$resourceGroupName,
+    
+        [Parameter(Mandatory = $True, HelpMessage = 'Specify the resource to get access token for')]
+        [String]$automationAccountName
+    )
+
+    $url = "https://login.microsoftonline.com/{0}/oauth2/token"
+    $resource = "https://management.azure.com/"
+    $grant_type = "client_credentials"
+
+    $body = @{
+        'grant_type'    = $grant_type
+        'resource'      = $resource
+        'client_id'     = $clientId
+        'client_secret' = $clientSecret
+    
+    }
+
+    $uri = $url -f $tenantId
+    $params = @{
+        ContentType = 'application/x-www-form-urlencoded'
+        Headers     = @{
+            'accept' = 'application/json'
+        }
+        Method      = 'Post'
+        URI         = $uri
+        Body        = $body
+    }
+
+    $authZResponse = Invoke-RestMethod @params
+    $access_token = $authZResponse.access_token
+
+    $atmnurl = "https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Automation/automationAccounts/{2}/webhooks/generateUri?api-version=2015-10-31"
+
+    $uri = $atmnurl -f $subscriptionId, $resourceGroupName, $automationAccountName
+    $params = @{
+        ContentType = "application/json"
+        Headers     = @{
+            "accept"        = "application/json"
+            "Authorization" = "Bearer $access_token"
+        }
+        Method      = "Post"
+        URI         = $uri    
+    }
+
+    $authZResponse = Invoke-RestMethod @params
+    Write-Verbose "Webhook Uri is $authZResponse"
+    return $authZResponse
+}
+
+function Get-VariableValues {
+    param (
+        [object]$value
+    )
     $tenantId = (Get-ProjectParameter -type "tenant").id
     $analysisServicesName = Get-FormatedText -strFormat "{0}{1}{2}as01"
     
@@ -31,7 +134,15 @@ function Set-ResourceSpecificParameters {
         [object]$resource,
         [object]$resourceparam
     )
-    $val = Get-Values($resourceparam.value)    
+    if ($resourceparam.name -eq "variableValues") {
+        $val = Get-VariableValues -value $resourceparam.value
+    }
+    elseif ($resourceparam.name -eq "webhookUri") {        
+        $val = Get-WebhookUri -resourcename $resource.name
+    }
+    else {
+        throw "resource param $($resourceparam.name) not supported"
+    } 
     return $val
 }
 
