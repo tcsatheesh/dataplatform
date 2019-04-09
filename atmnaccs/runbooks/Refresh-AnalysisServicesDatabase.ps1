b071e2fc18bf0118a73fd5387a613b8ea1000051
tcsatheesh <tc.satheesh@microsoft.com>
2019-04-10 00:26:51 +0100

    data factory calling automation account

--- atmnaccs/runbooks/Refresh-AnalysisServicesDatabase.ps1
+++ atmnaccs/runbooks/Refresh-AnalysisServicesDatabase.ps1
@@ -1,10 +1,48 @@
-$Credential = Get-AutomationPSCredential -Name "ServicePrincipal"
+param
+(
+    [Parameter (Mandatory = $false)]
+    [object] $WebhookData
+)
+    
+# If runbook was called from Webhook, WebhookData will not be null.
+if ($WebhookData) {	
+    # Check header for message to validate request
+    if ($WebhookData.RequestHeader.message -eq 'StartedbyDataFactory') {
+        Write-Output "Header has required information"
+    }
+    else {
+        Write-Output "Header missing required information";
+        exit;
+    }
+    
+    # Retrieve VM's from Webhook request body
+    $atmParameters = (ConvertFrom-Json -InputObject $WebhookData.RequestBody)
+    Write-Output "CredentialName: $($atmParameters.CredentialName)"
+    Write-Output "AnalysisServicesDatabaseName: $($atmParameters.AnalysisServicesDatabaseName)"
+    Write-Output "AnalysisServicesServer: $($atmParameters.AnalysisServicesServer)"
+    Write-Output "AnalysisServicesRolloutEnvironment: $($atmParameters.AnalysisServicesRolloutEnvironment)"
+    Write-Output "DatabaseRefreshType: $($atmParameters.DatabaseRefreshType)"
+    
+    $Credential = Get-AutomationPSCredential -Name $atmParameters.CredentialName
+    
+    $TenantId = $atmParameters.TenantId
+    $databaseName = $atmParameters.AnalysisServicesDatabaseName
+    $analysisServer = $atmParameters.AnalysisServicesServer
+    $analysisSeverRolloutEnvironment = $atmParameters.AnalysisServicesRolloutEnvironment
+    $refreshType = $atmParameters.DatabaseRefreshType
+    
+    Add-AzureAnalysisServicesAccount -Credential $Credential -ServicePrincipal -TenantId $TenantId -RolloutEnvironment $analysisSeverRolloutEnvironment
+    Invoke-ProcessASDatabase -DatabaseName $databaseName -RefreshType $refreshType -Server $analysisServer
+}
+else {
+    $Credential = Get-AutomationPSCredential -Name "ServicePrincipal"
 
-$TenantId = Get-AutomationVariable -Name "TenantId"
-$databaseName = Get-AutomationVariable -Name "AnalysisServicesDatabaseName"
-$analysisServer = Get-AutomationVariable -Name "AnalysisServicesServer"
-$analysisSeverRolloutEnvironment = Get-AutomationVariable -Name "AnalysisServicesRolloutEnvironment"
-$refreshType = Get-AutomationVariable -Name "DatabaseRefreshType"
+    $TenantId = Get-AutomationVariable -Name "TenantId"
+    $databaseName = Get-AutomationVariable -Name "AnalysisServicesDatabaseName"
+    $analysisServer = Get-AutomationVariable -Name "AnalysisServicesServer"
+    $analysisSeverRolloutEnvironment = Get-AutomationVariable -Name "AnalysisServicesRolloutEnvironment"
+    $refreshType = Get-AutomationVariable -Name "DatabaseRefreshType"
 
-Add-AzureAnalysisServicesAccount -Credential $Credential -ServicePrincipal -TenantId $TenantId -RolloutEnvironment $analysisSeverRolloutEnvironment
-Invoke-ProcessASDatabase -DatabaseName $databaseName -RefreshType $refreshType -Server $analysisServer
+    Add-AzureAnalysisServicesAccount -Credential $Credential -ServicePrincipal -TenantId $TenantId -RolloutEnvironment $analysisSeverRolloutEnvironment
+    Invoke-ProcessASDatabase -DatabaseName $databaseName -RefreshType $refreshType -Server $analysisServer
+}
--- dfpipelines/ps/New-Resources.ps1
+++ dfpipelines/ps/New-Resources.ps1
@@ -4,18 +4,136 @@ param
     [String]$projectsParameterFile
 )
 
-# function New-Resource {
-#     param (
-#         [object]$resource
-#     )
-    
-#     if ($resource.type -eq "salesforce") {
-#         & "$PSScriptRoot\New-Salesforce.ps1" -resource $resource
-#     }
-# }
+function Get-WebhookUri {
+    param(
+        [string]$resourcename
+    )
+    $subscriptionId = Get-SubscriptionId
+    $tenantId = (Get-ProjectParameter -type "tenant").id
+    $client = Get-ApplicationParameter -type "applicationPrincipal" -godeep
+    $clientId = $client.application.clientId
+    $clientSecretName = $client.application.passwordSecretName
+    $keyVaultName = Get-KeyVaultName -keyVaultType "appkeyvault"
+    Write-Verbose "Key Vault name: $keyVaultName"
+    $secret = Get-AzureKeyVaultSecret -VaultName $keyVaultName -Name $clientSecretName
+    $clientSecret = $secret.SecretValueText
+    $resourceGroupName = Get-ResourceGroupName -resourceGroupTypeRef "app"
+    $automationAccountName = $resourcename
 
-# $commonPSFolder = (Get-Item -Path "$PSScriptRoot\..\..\common\ps").FullName
-# & "$commonPSFolder\Invoke-NewProcess.ps1" -projectsParameterFile $projectsParameterFile -resourceType (Get-Item -Path $PSScriptRoot).Parent.Name -parameterFileName "$((Get-Item -Path $PSScriptRoot).Parent.Name).parameters.json"
+    $parameters = @{
+        "subscriptionId" = $subscriptionId
+        "tenantId" = $tenantId
+        "clientId" = $clientId
+        "clientSecret" = $clientSecret
+        "resourceGroupName" = $resourceGroupName
+        "automationAccountName" = $automationAccountName
+    }
+    
+    $val = Get-WebhookUriInternal   -subscriptionId $parameters.subscriptionId `
+                                    -tenantId $parameters.tenantId `
+                                    -clientId $parameters.clientId `
+                                    -clientSecret $parameters.clientSecret `
+                                    -resourceGroupName $parameters.resourceGroupName `
+                                    -automationAccountName $parameters.automationAccountName
+    return $val
+}
+
+
+function Get-WebhookUriInternal {
+    param (
+        [Parameter(Mandatory = $True, HelpMessage = 'Subscription id')]
+        [String]$subscriptionId,
+    
+        [Parameter(Mandatory = $True, HelpMessage = 'Tenant id')]
+        [String]$tenantId,
+    
+        [Parameter(Mandatory = $True, HelpMessage = 'Specify the client Id.')]
+        [String]$clientId,
+        
+        [Parameter(Mandatory = $True, HelpMessage = 'Specify the client secret.')]
+        [String]$clientSecret,
+    
+        [Parameter(Mandatory = $True, HelpMessage = 'Specify the resource to get access token for')]
+        [String]$resourceGroupName,
+    
+        [Parameter(Mandatory = $True, HelpMessage = 'Specify the resource to get access token for')]
+        [String]$automationAccountName
+    )
+
+    $url = "https://login.microsoftonline.com/{0}/oauth2/token"
+    $resource = "https://management.azure.com/"
+    $grant_type = "client_credentials"
+
+    $body = @{
+        'grant_type'    = $grant_type
+        'resource'      = $resource
+        'client_id'     = $clientId
+        'client_secret' = $clientSecret
+    
+    }
+
+    $uri = $url -f $tenantId
+    $params = @{
+        ContentType = 'application/x-www-form-urlencoded'
+        Headers     = @{
+            'accept' = 'application/json'
+        }
+        Method      = 'Post'
+        URI         = $uri
+        Body        = $body
+    }
+
+    $authZResponse = Invoke-RestMethod @params
+    $access_token = $authZResponse.access_token
+
+    $atmnurl = "https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Automation/automationAccounts/{2}/webhooks/generateUri?api-version=2015-10-31"
+
+    $uri = $atmnurl -f $subscriptionId, $resourceGroupName, $automationAccountName
+    $params = @{
+        ContentType = "application/json"
+        Headers     = @{
+            "accept"        = "application/json"
+            "Authorization" = "Bearer $access_token"
+        }
+        Method      = "Post"
+        URI         = $uri    
+    }
+
+    $authZResponse = Invoke-RestMethod @params
+    Write-Verbose "Webhook Uri is $authZResponse"
+    return $authZResponse
+}
+
+function Get-Body {
+    param (
+        [object]$value
+    )
+    Write-Verbose "************* $value"
+    $tenantId = (Get-ProjectParameter -type "tenant").id
+    $analysisServicesName = Get-FormatedText -strFormat "{0}{1}{2}as01"
+    
+    $value.TenantId = $value.TenantId -f $tenantId
+    $value.AnalysisServicesServer = $value.AnalysisServicesServer -f $analysisServicesName
+    
+    return $value
+}
+function Set-ResourceSpecificParameters {
+    param (
+        [object]$resource,
+        [object]$resourceparam
+    )
+    if ($resourceparam.name -eq "body") {
+        $val = Get-Body -value $resourceparam.value
+    }
+    elseif ($resourceparam.name -eq "url") {
+        $atmnaccname = Get-FormatedText -strFormat $resourceparam.value
+        $val = Get-WebhookUri -resourcename $atmnaccname
+    }
+    else {
+        throw "resource param $($resourceparam.name) not supported"
+    } 
+    return $val
+}
 
 
 $commonPSFolder = (Get-Item -Path "$PSScriptRoot\..\..\common\ps").FullName
